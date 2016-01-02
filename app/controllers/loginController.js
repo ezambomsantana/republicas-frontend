4a7796650bc48914379b75643c2a42c58a845f2d
Luiz Henrique Zambom Santana <lhzsantana@hotmail.com>
2016-01-02 11:14:26 -0200

    General Makeup for the PRView customization

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,10 +1,40 @@
-
 angular.module("myApp").controller("LoginController", [
-	'$scope','$location',
-    function($scope, $location){
+    '$scope', '$location', '$http',
+    function ($scope, $location, $http) {
 
-        $scope.login = function(){
-            $location.path("main");
+        $scope.login = function () {
+            var data = $.param({
+                json: JSON.stringify({
+                    email: $scope.email,
+                    password: $scope.password
+                })
+            });
+
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+
+            $http
+                .post
+                (
+                    "http://localhost:8080/users/login",
+                    data,
+                    config
+                )
+                .success(function (data, status) {
+                    console.log(response.data);
+                    $location.path("priview");
+                    //$location.path("main");
+                })
+                .error(function (data, status) {
+                    $scope.message='Erro ao realizar login';
+                })
         }
     }
-]);
+])
+;
